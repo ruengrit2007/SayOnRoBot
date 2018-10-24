9b1ee3581bfa141619f7f83b661aa8a7072fbc9b
ruengrit2007 <44318106+ruengrit2007@users.noreply.github.com>
2018-10-24 22:37:00 +0700

    Update line.php

--- line.php
+++ line.php
@@ -6,7 +6,7 @@ function send_LINE($msg){
   $messages = [
         'type' => 'text',
         'text' => $msg
-        //'text' => $text
+        'text' => $text
       ];
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
