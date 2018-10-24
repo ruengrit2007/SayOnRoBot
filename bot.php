9edd0d86ea97de65190b808e3d00e62dd995a632
ruengrit2007 <44318106+ruengrit2007@users.noreply.github.com>
2018-10-24 22:43:51 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,54 @@
 <?php
- require("pub.php");
+  function pubMqtt($topic,$msg){
+    $APPID= "SayOn/"; //enter your appid
+    $KEY = "ALVaMBoxFRm7MrO"; //enter your key
+    $SECRET = "EYaq59mVqT5N0Py8XUhj8ASRh"; //enter your secret
+    $Topic = "$topic"; 
+      put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
+ 
+  }
+ function getMqttfromlineMsg($Topic,$lineMsg){
+ 
+    $pos = strpos($lineMsg, ":");
+    if($pos){
+      $splitMsg = explode(":", $lineMsg);
+      $topic = $splitMsg[0];
+      $msg = $splitMsg[1];
+      pubMqtt($topic,$msg);
+    }else{
+      $topic = $Topic;
+      $msg = $lineMsg;
+      pubMqtt($topic,$msg);
+    }
+  }
+ 
+  function put($url,$tmsg)
+{
+      
+    $ch = curl_init($url);
+ 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+     
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+     
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
+ 
+    //curl_setopt($ch, CURLOPT_USERPWD, "ALVaMBoxFRm7MrO:EYaq59mVqT5N0Py8XUhj8ASRh");
+     
+    $response = curl_exec($ch);
+    
+      curl_close($ch);
+      echo $response . "\r\n";
+    return $response;
+}
+ //$Topic = "NodeMCU1";
+ //$lineMsg = "CHECK";
+ //getMqttfromlineMsg($Topic,$lineMsg);
+
 
  function send_LINE($msg){
  $access_token = 'pUSNzzZen370jm5bI5kLCmfbikfsjNmZ91BDlseT7uqm4psB6dUFRa9iC8grOXVCzHahbJoufFH/ez6U0j0ZcwHjX9tiXs3k4fECDbuQNOArtHhAl0BOm4GLen8gM12h8+YzOdqNAmdGMy+UTtxn9gdB04t89/1O/w1cDnyilFU='; 
