ee044c013a3c03a3b94f8fbc3ceba5add548631b
ruengrit2007 <44318106+ruengrit2007@users.noreply.github.com>
2019-08-03 21:47:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@
    if($message == "On"){
       $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
-      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าา";
       $arrayPostData['messages'][1]['type'] = "sticker";
       $arrayPostData['messages'][1]['packageId'] = "2";
       $arrayPostData['messages'][1]['stickerId'] = "34";
